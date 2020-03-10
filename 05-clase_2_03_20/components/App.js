1f4b2f0cdece7a49cd357c22d34205b0e641f188
505 <505@academicos.cesde.edu.co>
2020-03-09 20:16:01 -0500

    se actualizaron todos los ejercicios

--- 05-clase_2_03_20/components/App.js
+++ 05-clase_2_03_20/components/App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, Alert} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert} from 'react-native'
 
 export default function App() {
   const saludo = () => {Alert.alert("Hola Cesde")};
@@ -9,8 +9,11 @@ export default function App() {
     <View style={styles.container}>
       <Text>{name}</Text>
       <Image style={styles.logo} source={require('./assets/login.png')}/>
-      <TextInput maxLength={5} style={styles.TextInput} placeholder="uusuario" onChangeText={(text)=>setname(text)}/>
-      <Button title="start" onPress={saludo}></Button>
+      <TextInput maxLength={5} style={styles.TextInput} placeholder="Email" onChangeText={(text)=>setname(text)}/>
+      <TextInput maxLength={5} style={styles.TextInput} placeholder="Password" onChangeText={(text)=>setname(text)}/>
+      <Text style={styles.Text}>create Account</Text>
+      <Text style={styles.Text}>forgot password</Text>
+      <Button type="button" class="btn btn-secondary" title="Get started" onPress={saludo}></Button>
     </View>
   );
 }
@@ -18,10 +21,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#676463',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 logo:{
   width: 100,
   height: 100
@@ -29,10 +33,22 @@ logo:{
 TextInput: {
   borderColor: '#000000',
   borderWidth: 1,
-  borderRadius: 5,
-  paddingLeft: 20,
-  width: 300,
+  borderRadius: 20,
+  marginTop: 15,
+  marginBottom: 15,
+  paddingLeft: 15,
+  height: 50,
+  width: 400,
   textAlign: 'center'
-}
+},
+Text: {
+  marginTop: 15,
+  marginBottom: 15,
+  paddingLeft: 15,
+  height: 50,
+  width: 400,
+  textAlign: 'left',
+  color:'#A4A4A4'
+},
 
 });
